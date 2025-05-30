4a3f61d6d6fdf03dde0ec40cc56339e07507f362
mecoder110 <er.murtaza110@gmail.com>
2025-05-30 17:33:21 +0530

    Add code

--- src/com/dsa/random/RearrangeAtjecentSortedArray.java
+++ src/com/dsa/random/RearrangeAtjecentSortedArray.java
@@ -5,26 +5,38 @@ import java.util.Arrays;
 public class RearrangeAtjecentSortedArray {
 
 	public static void main(String[] args) {
-		int[] arr = {1,2,3,4,5,6,7,8,9};
-		
+		int[] arr = { 1,1,1,2,2,2,3,3,3 };
+
 		int[] res = reArrangeAtjecent(arr);
 		System.out.println(Arrays.toString(res));
+
+		xyz(1);
+
+	}
+
+	private static void xyz(char c) {
+		System.out.println("char");
+
+	}
+
+	private static void xyz(int c) {
+		System.out.println("int");
 
 	}
 
 	private static int[] reArrangeAtjecent(int[] arr) {
-		
-		int n =arr.length;
-		
-		int i=0;
-		//int j=i+1;
-		while(i<n-1) {
-				int temp = arr[i];
-				arr[i] = arr[i+1];
-				arr[i+1] = temp;
-				i+=2;
+
+		int n = arr.length;
+
+		int i = 0;
+		// int j=i+1;
+		while (i < n - 1) {
+			int temp = arr[i];
+			arr[i] = arr[i + 1];
+			arr[i + 1] = temp;
+			i += 2;
 		}
-		
+
 		return arr;
 	}
 
